466b86c39f27691c5196edb7f87ae6aef2033941
Nathan Moon <moonman700@gmail.com>
2023-02-19 14:27:11 -0500

    added begin button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,30 @@
 var timeEl = document.querySelector(".time");
 var secondsLeft = 300
+var begin = document.getElementById('begin');
+var beginButton = document.getElementById('beginButton');
 
-var Begin = document.querySelector("#Begin");
-var beginningText = document.querySelector("beginningText")
+var questions = document.getElementById('questions');
+var scoreboard = document.getElementById('scoreboard');
+var results = document.getElementById('results')
+
 
 function setTime() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = "Time: " + secondsLeft;
-  
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-        sendMessage();
-      }
-  
-    }, 1000);
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
 }
-
-setTime()
-
-Begin.addEventListener("click", function() {
-    beginningText.remove()
-
-
-
+beginButton.addEventListener("click", function() {
+    begin.className = "hide"
+    questions.className = ""
+    setTime()
 })
