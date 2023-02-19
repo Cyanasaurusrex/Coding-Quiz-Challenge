2ad79942a567e2cadd59b7adfa2509fc0b7663e3
Nathan Moon <moonman700@gmail.com>
2023-02-19 17:22:21 -0500

    added object with question data to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,33 @@ var beginButton = document.getElementById('beginButton');
 var questions = document.getElementById('questions');
 var scoreboard = document.getElementById('scoreboard');
 var results = document.getElementById('results')
+let questions = [
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts'
+  }
+  {
+    title: 'The condition in an if /else statement is enclosed within ______ .',
+    choices: ['Quotes', 'Curly Brackets', 'Parentheses', 'Square Brackets'],
+    answer: 'Parenthesis'
+  }
+  {
+    title: 'Array in JavaScript can be used to store ______ .',
+    choices: ['Numbers and Strings', 'Other Arrays', 'Booleans', 'All of the Above'],
+    answer: 'All of the above'
+  }
+  {
+    title: 'String values must be enclosed within ______ when being assigned to variables',
+    choices: ['Commas', 'Curly Brackets', 'Quotes,', 'Parenthesis'],
+    answer: 'Quotes'
+  }
+  {
+    title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['Javascript', 'Terminal/bash', 'For Loops', 'console.log'],
+    answer: 'console.log'
+  }
+]
 
 
 function setTime() {
@@ -27,4 +54,5 @@ beginButton.addEventListener("click", function() {
     begin.className = "hide"
     questions.className = ""
     setTime()
+    
 })
