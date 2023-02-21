98c653e94cf56ebbfef9898ffca16192ceeef241
Nathan Moon <moonman700@gmail.com>
2023-02-21 01:07:39 -0500

    simplified logic for javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
-var timeEl = document.querySelector(".time");
-var secondsLeft = 300
+var timeEl = document.getElementById('time');
+var secondsLeft = 15
 
 var beginButton = document.getElementById('beginButton');
-// variables for displaying question
+// variables for displaying question / answer
 var title = document.getElementById('title')
 var option1 = document.getElementById('option1');
 var option2 = document.getElementById('option2');
 var option3 = document.getElementById('option3');
 var option4 = document.getElementById('option4');
+var verify = document.getElementById('verify')
 
 // variables for different stages of quiz
 var begin = document.getElementById('begin');
@@ -17,6 +18,7 @@ var results = document.getElementById('results')
 
 // variables for logical operations
 var questionNumber = 0
+var score = 0
 
 let questions = [
   {
@@ -56,6 +58,9 @@ function setTime() {
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
+      quiz.className = "hide"
+      timeEl.className = "hide"
+      results.className = ""
       // Calls function to create and append image
       sendMessage();
     }
@@ -63,73 +68,68 @@ function setTime() {
   }, 1000);
 }
 
-
-
-
 // displays the next question
 function displayQuestion() {
-  title.textContent = questions[questionNumber].title
-  option1.textContent = questions[questionNumber].choices[0]
-  option2.textContent = questions[questionNumber].choices[1]
-  option3.textContent = questions[questionNumber].choices[2]
-  option4.textContent = questions[questionNumber].choices[3]
-    
+  if (questionNumber  == questions.length) {
+    quiz.className = "hide"
+    timeEl.className = "hide"
+    results.className = ""
+  }
+  else{
+    title.textContent = questions[questionNumber].title
+    option1.textContent = questions[questionNumber].choices[0]
+    option2.textContent = questions[questionNumber].choices[1]
+    option3.textContent = questions[questionNumber].choices[2]
+    option4.textContent = questions[questionNumber].choices[3]
+  }
 }
+    
+// checks and displays results from previous question and shows next question
+function checkAnswer() {
+  if (questions[questionNumber].choices[chosenAnswer] === questions[questionNumber].answer){
+    verify.textContent = "Your answer for question " + (questionNumber + 1) +" is correct!"    
+  }
+  else {
+    verify.textContent = "Your answer for question " + (questionNumber + 1) + " is wrong!"    
+  }  
+  
+  questionNumber = questionNumber + 1
+  displayQuestion()
+
+  return questionNumber
+}
+
+
+// event listeners record the selected response and call the check answer function
+
+option1.addEventListener("click", function() {
+  chosenAnswer = 0
+  checkAnswer()  
+})
+
+option2.addEventListener("click", function() {
+  chosenAnswer = 1
+  checkAnswer()
+})
+
+option3.addEventListener("click", function() {
+  chosenAnswer = 2
+  checkAnswer()
+})
+
+
+option4.addEventListener("click", function() {
+  chosenAnswer = 3
+  checkAnswer()
+})
+
 
 // removes the beginning prompt, shows the first question + answers, 
 // and starts the timer
-
 
 beginButton.addEventListener("click", function() {
   begin.className = "hide"
   quiz.className = ""  
   setTime();
   displayQuestion();
-})
-
-option1.addEventListener("click", function() {
-  if (option1.textContent === questions[questionNumber].answer){
-    alert("correct")
-  }
-  else {
-    alert("wrong")
-  }
-  questionNumber = questionNumber + 1
-  displayQuestion()
-  }
-)
-
-// event listeners
-
-option2.addEventListener("click", function() {
-  if (option2.textContent === questions[questionNumber].answer){
-    alert("correct")
-  }
-  else {
-    alert("wrong")
-  }
-  questionNumber = questionNumber + 1
-  displayQuestion()
-})
-
-option3.addEventListener("click", function() {
-  if (option3.textContent === questions[questionNumber].answer){
-    alert("correct")
-  }
-  else {
-    alert("wrong")
-  }
-  questionNumber = questionNumber + 1
-  displayQuestion()
-})
-
-option4.addEventListener("click", function() {
-  if (option4.textContent === questions[questionNumber].answer){
-    alert("correct")
-  }
-  else {
-    alert("wrong")
-  }
-  questionNumber = questionNumber + 1
-  displayQuestion()
 })
